689c99ad4ad75d0414b2a502f2f87f2d21317397
Aadcode <118838917+Aadcode@users.noreply.github.com>
2024-09-03 19:49:14 +0530

    Experience Section Modified

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -8,43 +8,45 @@ export const Experience = () => {
         whileInView={{ opacity: 1, x: 0 }}
         initial={{ opacity: 0, x: -100 }}
         transition={{ duration: 0.5 }}
-        className="my-10 sm:my-20 text-center text-3xl sm:text-4xl md:text-5xl"
+        className="my-10 sm:my-20 text-center text-3xl sm:text-4xl md:text-5xl font-bold"
       >
         Training
       </motion.h1>
       <div>
         {EXPERIENCES.map((experience, index) => (
-          <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+          <div
+            key={index}
+            className="mb-8 flex flex-col items-center sm:flex-row lg:justify-center"
+          >
             <motion.div
               whileInView={{ opacity: 1, x: 0 }}
               initial={{ opacity: 0, x: -100 }}
               transition={{ duration: 1 }}
-              className="w-full sm:w-1/3 lg:w-1/4 flex justify-center lg:justify-start"
+              className="w-full sm:w-1/3 lg:w-1/4 flex justify-center mb-4 sm:mb-0"
             >
               <img
                 src={experience.image}
-                width={160}
-                height={160}
-                className="mb-6 rounded-full sm:rounded-lg"
+                alt={experience.year}
+                className="w-28 h-28 sm:w-40 sm:h-40 rounded-lg shadow-lg"
               />
             </motion.div>
             <motion.div
               whileInView={{ opacity: 1, x: 0 }}
               initial={{ opacity: 0, x: -100 }}
               transition={{ duration: 1 }}
-              className="w-full sm:w-2/3 lg:w-3/4 max-w-xl"
+              className="w-full sm:w-2/3 lg:w-3/4 px-4 lg:px-8"
             >
-              <h6 className="mb-2 font-semibold text-lg sm:text-xl">
+              <h6 className="mb-2 text-lg sm:text-xl font-semibold text-center sm:text-left">
                 {experience.year}
               </h6>
-              <p className="mb-4 text-neutral-400 text-sm sm:text-base">
+              <p className="mb-4 text-neutral-400 text-sm sm:text-base text-center sm:text-left">
                 {experience.description}
               </p>
-              <div className="flex flex-wrap">
+              <div className="flex flex-wrap justify-center sm:justify-start">
                 {experience.technologies.map((tech, techIndex) => (
                   <span
                     key={techIndex}
-                    className="mr-2 mb-2 rounded bg-neutral-900 px-2 py-1 text-xs sm:text-sm font-medium text-purple-900"
+                    className="mr-2 mb-2 rounded bg-neutral-900 px-2 py-1 text-xs sm:text-sm font-medium text-purple-400"
                   >
                     {tech}
                   </span>
