fa1525ebbdb57e23797624989b179eb841c02d5f
Aadcode <118838917+Aadcode@users.noreply.github.com>
2024-09-03 19:45:37 +0530

    Project Section Modified

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -16,33 +16,33 @@ export const Project = () => {
         {PROJECTS.map((project, index) => (
           <div
             key={index}
-            className="mb-8 flex flex-col lg:flex-row lg:justify-start lg:items-start"
+            className="mb-8 flex flex-col items-center sm:flex-row lg:justify-center"
           >
             <motion.div
               whileInView={{ opacity: 1, x: 0 }}
               initial={{ opacity: 0, x: -100 }}
               transition={{ duration: 1 }}
-              className="flex-shrink-0 w-full sm:w-1/3 lg:w-1/4 flex justify-center lg:justify-start mb-4 lg:mb-0"
+              className="w-full sm:w-1/3 lg:w-1/4 flex justify-center mb-4 sm:mb-0"
             >
               <img
                 src={project.image}
                 alt={project.title}
-                className="w-32 h-32 sm:w-40 sm:h-40 rounded-lg shadow-lg"
+                className="w-28 h-28 sm:w-40 sm:h-40 rounded-lg shadow-lg"
               />
             </motion.div>
             <motion.div
               whileInView={{ opacity: 1, x: 0 }}
               initial={{ opacity: 0, x: -100 }}
               transition={{ duration: 1 }}
-              className="w-full sm:w-2/3 lg:w-3/4 px-4 lg:px-8 flex flex-col justify-center"
+              className="w-full sm:w-2/3 lg:w-3/4 px-4 lg:px-8"
             >
-              <h6 className="mb-2 text-lg sm:text-xl font-semibold">
+              <h6 className="mb-2 text-lg sm:text-xl font-semibold text-center sm:text-left">
                 {project.title}
               </h6>
-              <p className="mb-4 text-neutral-400 text-sm sm:text-base">
+              <p className="mb-4 text-neutral-400 text-sm sm:text-base text-center sm:text-left">
                 {project.description}
               </p>
-              <div className="flex flex-wrap">
+              <div className="flex flex-wrap justify-center sm:justify-start">
                 {project.technologies.map((tech, techIndex) => (
                   <span
                     key={techIndex}
